53e72363ba0642eaf2fb1059d278fb69b61421a6
TOIKIT <develop@toikit.com>
2025-08-30 20:01:00 +0700

    Update market place

---
+++ src/components/t-loading-app.vue
@@ -0,0 +1,19 @@
+<style lang="scss" scoped>
+.t-content{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
+
+<template>
+  <t-content>
+    <t-loading-icon type="spinner"/>
+  </t-content>
+</template>
+
+<script lang="ts" setup>
+import TContent from './t-content.vue';
+import TLoadingIcon from './t-loading-icon.vue';
+</script>
